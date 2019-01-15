8d671f788127233a4da27c782a013ad9c3e9d2ff
Matthew Ballard <mr.ballard@outlook.com>
2019-01-15 00:38:08 +0000

    Add projects and design changes

---
+++ assets/script.js
@@ -0,0 +1,15 @@
+alert("testing javascript");
+
+
+    
+wrapperClass = document.getElementByClassName("page-background"); 
+console.log(wrapperClass);
+
+ 
+//  if (document.URL.contains("/index.html")) {
+//     console.log("on home page");
+//     document.body.style.background-image = "url("/images/old-keyboard.png")";
+//   } else {
+//     console.log("not on home page");
+//     document.body.style.background-image = "";
+//   };
